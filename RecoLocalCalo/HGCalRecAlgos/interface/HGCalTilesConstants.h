7aed27f13f247909a64bef1fec07f5e6f1b1e2cb
Felice <felice.pantaleo@cern.ch>
2019-05-02 12:11:30 +0200

    indentation

--- RecoLocalCalo/HGCalRecAlgos/interface/HGCalTilesConstants.h
+++ RecoLocalCalo/HGCalRecAlgos/interface/HGCalTilesConstants.h
@@ -18,14 +18,14 @@ constexpr int32_t ceil(float num)
 }
 
   // first is for CE-E, second for CE-H in cm
-  constexpr std::array<float, 2> minX = {{-165.f, -265.f}}; 
-  constexpr std::array<float, 2> maxX = {{ 165.f,  265.f}}; 
+  constexpr std::array<float, 2> minX = {{-165.f, -265.f}};
+  constexpr std::array<float, 2> maxX = {{ 165.f,  265.f}};
   constexpr std::array<float, 2> minY = {{-165.f, -265.f}};
-  constexpr std::array<float, 2> maxY = {{ 165.f,  265.f}}; 
+  constexpr std::array<float, 2> maxY = {{ 165.f,  265.f}};
   constexpr float tileSize = 5.f;
   constexpr std::array<int, 2> nColumns = {{hgcalTilesConstants::ceil(maxX[0]-minX[0]/tileSize),
                                               hgcalTilesConstants::ceil(maxX[1]-minX[1]/tileSize)}};
-  constexpr std::array<int, 2> nRows = {{   hgcalTilesConstants::ceil(maxY[0]-minY[0]/tileSize),
+  constexpr std::array<int, 2> nRows    = {{hgcalTilesConstants::ceil(maxY[0]-minY[0]/tileSize),
                                               hgcalTilesConstants::ceil(maxY[1]-minY[1]/tileSize)}};
 
 }
